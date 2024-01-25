cd231f63eb12ce2a7874d6c8370c58bc1ea95c51
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-01-25 14:07:59 -0500

    feat: Add the template version of the util.py file.

---
+++ containmentcheck/containmentcheck/util.py
@@ -0,0 +1,6 @@
+"""Utility functions to support benchmarking containment in a collection."""
+
+
+def human_readable_boolean():
+    """Produce a human-readable Yes or No for a boolean value of True or False."""
+    return "No"
