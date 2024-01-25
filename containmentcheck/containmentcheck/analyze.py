6e499a4b37f4214bcb96f63acf22e9cfb6d28e08
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-01-25 13:59:18 -0500

    feat: Add the template version of the analyze.py file.

---
+++ containmentcheck/containmentcheck/analyze.py
@@ -0,0 +1,7 @@
+"""Analyze data values created from an experiment."""
+
+
+def calculate_average_values(data_list, data_count):
+    # TODO: Calculate the average values for the data in the provided list
+    data_list_averages = []
+    return data_list_averages
