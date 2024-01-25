ffd41de1a659654f0138db58586474c4c4ab2c85
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-01-25 14:05:40 -0500

    feat: Add the template version of the generate.py file.

---
+++ containmentcheck/containmentcheck/generate.py
@@ -0,0 +1,22 @@
+"""Generate random data values and collections as containers."""
+
+import random
+from typing import List, Tuple, Union
+
+
+def generate_random_number(maximum, exceed):
+    # generate a random number that is either up to the maximum
+    # or exceeding it by one, depending on the input parameter values
+    return 0
+
+
+def generate_random_container(
+    size,
+    maximum,
+    make_tuple,
+):
+    # TODO: generate a list of random values for a specific size
+    # and with a number up to a specific maximum
+    random_list = []
+    # TODO: convert the list to a tuple if this was requested
+    return random_list
